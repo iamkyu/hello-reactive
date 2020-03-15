0426bb76f264833390b33aa4ae14b3c2aa7d9b96
iamkyu <kyujnam@gmail.com>
2020-03-15 23:14:05 +0900

    feat: tech.io 플레이그라운드 ch11

---
+++ src/main/java/io/iamkyu/techio/Part10ReactiveToBlocking.java
@@ -0,0 +1,26 @@
+package io.iamkyu.techio;
+
+import io.iamkyu.techio.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Learn how to turn Reactive API to blocking one.
+ *
+ * @author Sebastien Deleuze
+ */
+public class Part10ReactiveToBlocking {
+
+//========================================================================================
+
+	User monoToValue(Mono<User> mono) {
+		return mono.block();
+	}
+
+//========================================================================================
+
+	Iterable<User> fluxToValues(Flux<User> flux) {
+		return flux.toIterable();
+	}
+
+}
